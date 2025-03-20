09dcaf524ac5e4000df1bbe78fbd0cdca26e5f0e
haithamassoli <haitham.b.assoli@gmail.com>
2025-03-20 10:24:04 +0300

    Add user details retrieval for specific reservation types in ReservationExpandedView

--- src/presentation/pages/clubAdmin/reservation/components/ReservationExpandedView.tsx
+++ src/presentation/pages/clubAdmin/reservation/components/ReservationExpandedView.tsx
@@ -1,4 +1,6 @@
+import { PlaysonicUsersQueryByIdInstance } from "@app/useCases/general/playsonicUsers/query/PlaysonicUsersQueryById";
 import { ReservationQueryByIdInstance } from "@app/useCases/reservation";
+import { authenticationURLEnum } from "@domain/enums";
 import { ReservationUrlEnum } from "@domain/enums/URL/Reservation/reservationUrls/Reservation";
 import { QUERIES } from "@presentation/helpers";
 import { FC } from "react";
@@ -13,9 +15,21 @@ export const ReservationExpandedView: FC<any> = ({ row }) => {
         row.original.id
       );
     },
+    enabled: row.original.reservationTypeId !== 4,
   });
 
-  if (isLoading) {
+  const { data, isLoading: isLoadingUser } = useQuery({
+    queryKey: [QUERIES.PlaysonicUsersList, row.original.ownerID],
+    queryFn: () => {
+      return PlaysonicUsersQueryByIdInstance.getPlaysonicUsersById(
+        authenticationURLEnum.getUser,
+        row.original.ownerID
+      );
+    },
+    enabled: row.original.reservationTypeId === 4,
+  });
+
+  if (isLoading || isLoadingUser) {
     return (
       <div className="px-8 py-4 bg-light-primary rounded mx-8 mb-4">
         <div className="d-flex justify-content-center">
@@ -36,12 +50,21 @@ export const ReservationExpandedView: FC<any> = ({ row }) => {
             </tr>
           </thead>
           <tbody>
-            {userReservationData?.data?.map((user) => (
-              <tr key={user.userId}>
-                <td>{user.fullName}</td>
-                <td className="tw-pl-6">{user.phone || "N/A"}</td>
+            {userReservationData &&
+              userReservationData?.data?.map((user) => (
+                <tr key={user.userId}>
+                  <td>{user.fullName}</td>
+                  <td className="tw-pl-6">{user.phone || "N/A"}</td>
+                </tr>
+              ))}
+            {data && (
+              <tr key={data.id}>
+                <td>
+                  {data.firstName} {data.lastName}
+                </td>
+                <td className="tw-pl-6">{data.phoneNo || "N/A"}</td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       </div>
